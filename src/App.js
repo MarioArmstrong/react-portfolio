20e58722792583b057ca72c353bf0a5ef7bda15e
MarioArmstrong <arms.mario@icloud.com>
2023-01-02 20:47:29 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import MyApps from "./components/MyApps";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <MyApps />
+    </div>
+  );
+}
+
+export default App;
